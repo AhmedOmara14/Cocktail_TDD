da73665ed3b791cdd57327e7b8ad98fc8b4f5cbe
AhmedOmara14 <ahmedomaea53@gmail.com>
2023-12-01 16:28:23 +0200

    implement fun of save Score

--- app/src/main/java/com/omaradev/cocktail/data/repository/CocktailRepositoryImpl.kt
+++ app/src/main/java/com/omaradev/cocktail/data/repository/CocktailRepositoryImpl.kt
@@ -3,8 +3,11 @@ package com.omaradev.cocktail.data.repository
 import android.content.SharedPreferences
 import com.omaradev.cocktail.domain.repository.CocktailRepository
 
-class CocktailRepositoryImpl(sharedPreferences: SharedPreferences) : CocktailRepository {
+class CocktailRepositoryImpl(private val sharedPreferences: SharedPreferences) : CocktailRepository {
+    private val highScoreKey = "HIGH_SCORE"
     override fun saveScore(score: Int) {
-        TODO("Not yet implemented")
+        val editor = sharedPreferences.edit()
+        editor.putInt(highScoreKey, score)
+        editor.apply()
     }
 }
